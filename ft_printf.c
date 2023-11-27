99c32f8aab65fde4a44817ef2b5a48ab1a7cdd8b
jou.dragopc <jou.42.rio@gmail.com>
2023-11-27 01:17:44 -0300

    ft_printf without pointer

--- ft_printf.c
+++ ft_printf.c
@@ -24,7 +24,7 @@ int	ft_printnbr(long va_arg, int base, char *set,  int u)
 	}
 	if (sig && !u)
 		str[i++] = '-';
-	len = i + sig;
+	count = i + sig;
 	while (i >= 0)
 		write (1, &str[--i], 1);
 	return (count);
@@ -46,14 +46,18 @@ int	ft_printchr(char *va_arg)
 int	ft_checkarg(va_list args, char flag)
 {
 	int	count;
+	char	c;
 
-	count = 0
+	count = 0;
 	if (flag == 'c')
-		count += write(1, &va_arg(args, char), 1);
+	{
+		c = va_arg(args, int);
+		count += write(1, &c, 1);
+	}
 	else if (flag == 'i' || flag == 'd')
 		count += ft_printnbr(va_arg(args, int), 10, "0123456789", 0);
 	else if (flag == 'u')
-		count += ft_printf(va_arg(args, unsigned int), 10, "0123456789", 1);
+		count += ft_printnbr(va_arg(args, unsigned int), 10, "0123456789", 1);
 	else if (flag == 's')
 		count += ft_printchr(va_arg(args, char *));
 	else if (flag == 'p')
@@ -63,42 +67,27 @@ int	ft_checkarg(va_list args, char flag)
 	else if (flag == 'X')
 		count += ft_printnbr(va_arg(args, int), 16, "0123456789ABCDEF", 1);
 	else if (flag == '%')
-		count += write(1, '%', 1);
-
+		count += write(1, "%", 1);
+	return (count);
 }
 
   
 int	ft_printf(const char *str, ...)
 {
 	int	i;
+	int	count;
 	va_list	args;
 
 	i = 0;
+	count = 0;
 	va_start(args, str);
 	while(str[i])
 	{
 		if (str[i] == '%')
-		{
-			i++;
-			if (str[i] == 'c')
-				write(1, &va_arg(args, char), 1);
-			else if (str[i] == 'i' || str[i++] == 'd')
-				ft_printnbr(va_arg(args, int), 10, "0123456789", 0);
-			else if (str[i] == 'u')
-				ft_printf(va_arg(args, unsigned int), 10, "0123456789", 1);
-			else if (str[i] == 's')
-				ft_printchr(va_arg(args, char *));
-			else if (str[i] == 'p')
-				ft_printchr(va_arg(args, void *));
-			else if (str[i] == 'x')
-				ft_printnbr(va_arg(args, int), 16, "0123456789abcdef", 1);
-			else if (str[i] == 'X')
-				ft_printnbr(va_arg(args, int), 16, "0123456789ABCDEF", 1);
-			else if (str[i] == '%')
-				write(1, '%', 1);
-		}
+			count += ft_checkarg(args, str[++i]);
 		else
 			write(1, &str[i], 1);
+		count++;
 		i++;
 	}
 	va_end(args);
@@ -106,6 +95,6 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	ft_printnbr(-1, 16, "0123456789abcdef", 1);
+	ft_printf("teste int %c", 't');
 	return (0);
 }
